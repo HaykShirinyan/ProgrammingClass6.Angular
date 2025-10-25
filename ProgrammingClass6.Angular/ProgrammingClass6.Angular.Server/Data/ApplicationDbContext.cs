1135ae79417f27480925e9b85981ecc6ec6bb079
Levon Avetisyan <levonavetisyan032@gmail.com>
2025-10-25 09:23:41 -0700

    Update ApplicationDbContext.cs

--- ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Data/ApplicationDbContext.cs
+++ ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ProgrammingClass6.Angular.Server.Models;
 
 namespace ProgrammingClass6.Angular.Server.Data
@@ -6,6 +6,8 @@ namespace ProgrammingClass6.Angular.Server.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet <ProductUnit> ProductUnits { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
