226dcd519966ecb054ae843b921e912ef196a6ce
Karen Hayrapetyan <karendyan@mail.ru>
2025-10-27 11:57:14 +0400

    Add new Unit_Of_Measure

---
+++ ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Controllers/Unit_Of_MeasureController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProgrammingClass6.Angular.Server.Data;
+using ProgrammingClass6.Angular.Server.Models;
+
+namespace ProgrammingClass6.Angular.Server.Controllers
+{
+    [Route("api/unit_of_measures")]
+    [ApiController]
+    public class Unit_Of_MeasureController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public Unit_Of_MeasureController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var unitOfMeasures = _dbcontext.Unit_Of_Measures.ToList();
+            return Ok(unitOfMeasures);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var unitOfMeasure = _dbcontext.Unit_Of_Measures.SingleOrDefault(uom => uom.Id == id);
+            if (unitOfMeasure == null)
+            {
+                return NotFound();
+            }
+            return Ok(unitOfMeasure);
+        }
+        [HttpPost]
+        public IActionResult Create(Unit_Of_Measure unit_Of_measure)
+        {
+             _dbcontext.Unit_Of_Measures.Add(unit_Of_measure);
+            _dbcontext.SaveChanges();
+            return Ok(unit_Of_measure);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Unit_Of_Measure unit_Of_Measure)
+        {
+            if (id != unit_Of_Measure?.Id)
+            {
+                return BadRequest("Id in the body doesn't match with Id in the URL.");
+            }
+            _dbcontext.Unit_Of_Measures.Update(unit_Of_Measure);
+            _dbcontext.SaveChanges();
+            return Ok(unit_Of_Measure);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var unitOfMeasure = _dbcontext.Unit_Of_Measures.SingleOrDefault(uom => uom.Id == id);
+            if (unitOfMeasure != null)
+            {
+                _dbcontext.Unit_Of_Measures.Remove(unitOfMeasure);
+                _dbcontext.SaveChanges();
+                return Ok(unitOfMeasure);
+            }
+            return NotFound();
+        }
+
+    }
+}
--- ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Data/Migrations/20251027073318_Unit_Of_Measures.Designer.cs
+++ ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Data/Migrations/20251027073318_Unit_Of_Measures.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProgrammingClass6.Angular.Server.Data;
 
@@ -10,9 +11,11 @@ using ProgrammingClass6.Angular.Server.Data;
 namespace ProgrammingClass6.Angular.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251027073318_Unit_Of_Measures")]
+    partial class Unit_Of_Measures
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
