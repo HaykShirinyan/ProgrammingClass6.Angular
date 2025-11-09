f1c855b387ac9430285161ff162c34c25b007a5d
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-11-09 11:16:30 -0800

    Added ProductService and ProductRepository

---
+++ ProgrammingClass6.Angular/ProgrammingClass6.Angular.Server/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,60 @@
+﻿using ProgrammingClass6.Angular.Server.Data;
+using ProgrammingClass6.Angular.Server.Models;
+using ProgrammingClass6.Angular.Server.Repositories.Definitions;
+using System.Threading.Tasks;
+
+namespace ProgrammingClass6.Angular.Server.Repositories.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ProductRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _dbContext
+                .Products
+                .ToList();
+        }
+
+        public Product Get(int id)
+        {
+            return _dbContext
+                .Products
+                .SingleOrDefault(product => product.Id == id);
+        }
+
+        public Product Add(Product product)
+        {
+            _dbContext.Products.Add(product);
+            _dbContext.SaveChanges();
+
+            return product;
+        }
+
+        public Product Update(Product product)
+        {
+            _dbContext.Products.Update(product);
+            _dbContext.SaveChanges();
+
+            return product;
+        }
+
+        public Product Delete(int id)
+        {
+            var product = Get(id);
+
+            if (product != null)
+            {
+                _dbContext.Products.Remove(product);
+                _dbContext.SaveChanges();
+            }
+
+            return product;
+        }
+    }
+}
