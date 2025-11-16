00b3f5df0eca3ed179bd1cee7a3b0514bd0963ed
Hayk Shirinyan <hayk.shirinyan@developerpartners.com>
2025-11-16 11:36:06 -0800

    Added create and edit product pages

--- ProgrammingClass6.Angular/programmingclass6.angular.client/src/app/app-routing.module.ts
+++ ProgrammingClass6.Angular/programmingclass6.angular.client/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './products/list/product-list.component';
+import { CreateProductComponent } from './products/create/create-product.component';
+import { EditProductComponent } from './products/edit/edit-product.component';
 
 const routes: Routes = [
   {
     path: 'products',
     component: ProductListComponent
+  },
+  {
+    path: 'products/create',
+    component: CreateProductComponent
+  },
+  {
+    path: 'products/edit/:id',
+    component: EditProductComponent
   }
 ];
 
--- ProgrammingClass6.Angular/programmingclass6.angular.client/src/app/app.module.ts
+++ ProgrammingClass6.Angular/programmingclass6.angular.client/src/app/app.module.ts
@@ -5,15 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/list/product-list.component';
+import { CreateProductComponent } from './products/create/create-product.component';
+import { FormsModule } from '@angular/forms';
+import { EditProductComponent } from './products/edit/edit-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    CreateProductComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi())
